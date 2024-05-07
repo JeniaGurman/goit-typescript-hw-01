593a333d73b3b525c943a3932b67686a9a38c01a
JeniaGurman <144277083+JeniaGurman@users.noreply.github.com>
2024-05-07 23:12:20 +0300

    homework 1

---
+++ vite-project/src/basic/3.ts
@@ -0,0 +1,9 @@
+let mixedType: string | number;
+mixedType = `Hello`;
+mixedType = 10;
+
+type EnableOrDisable = `enable` | `disable`;
+let answer: EnableOrDisable;
+answer = `enable`;
+answer = "disable";
+export { };
